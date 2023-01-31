6243383df086336bb59a2e9e515cb8235dd34026
yasunoriiii <82689771+yasunoriiii@users.noreply.github.com>
2023-01-31 14:47:05 +0900

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+//スクロールすると上部に固定させるための設定を関数でまとめる
+function FixedAnime() {
+	var headerH = $('#header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//headerの高さ以上になったら
+			$('#header').addClass('HeightMin');//fixedというクラス名を付与
+			}else{//それ以外は
+			$('#header').removeClass('HeightMin');//fixedというクラス名を除去
+		}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
+
+$('.slider').slick({
+    arrows: false,//左右の矢印はなし
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+    speed: 6900,//スライドのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+    pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+    cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+    slidesToShow: 4,//スライドを画面に4枚見せる
+    slidesToScroll: 1,//1回のスライドで動かす要素数
+    responsive: [
+        {
+        breakpoint: 769,//モニターの横幅が769px以下の見せ方
+        settings: {
+            slidesToShow: 2,//スライドを画面に2枚見せる
+        }
+    },
+    {
+        breakpoint: 426,//モニターの横幅が426px以下の見せ方
+        settings: {
+            slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+        }
+    }
+]
+});
